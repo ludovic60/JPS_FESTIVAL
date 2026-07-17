01ac557a079332be066915c12b37c1aff082368a
JPS_TMP <57634868+ludovic60@users.noreply.github.com>
2026-07-17 09:47:33 +0200

    Update main_weekend_app.py

--- app/main_weekend_app.py
+++ app/main_weekend_app.py
@@ -5,7 +5,7 @@ Lancement local :  streamlit run streamlit_app.py
 import streamlit as st
 
 from design_system import inject
-from weekend_app import auth, storage , views
+from weekend_app import auth, storage, views
 ##from weekend_app.views import login_view, reset_password_view, main_app
 
 st.set_page_config(page_title="Présence Week-end", page_icon="📅", layout="wide")
