680d6afde677fcd0e2ee37dc5fbc2a7f9a4a52be
JPS_TMP <57634868+ludovic60@users.noreply.github.com>
2026-08-21 14:37:46 +0200

    Update auth.py

--- app/bar_jeux_app/auth.py
+++ app/bar_jeux_app/auth.py
@@ -62,4 +62,4 @@ def logout():
 
 
 def _public(u):
-    return {"id": u["_id"], "email": u["email"], "name": u["name"], "role": u["role"]}
+    return {"id": str(u["_id"]), "email": u["email"], "name": u["name"], "role": u["role"]}
