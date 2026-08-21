d8b9abd716d78d776a08bbd22c037b9f0f0c7be2
JPS_TMP <57634868+ludovic60@users.noreply.github.com>
2026-08-21 11:17:59 +0200

    Update views.py

--- app/bar_jeux_app/views.py
+++ app/bar_jeux_app/views.py
@@ -68,16 +68,16 @@ def _password_page(user):
     password_input = st.text_input("Mot de passe à hacher", type="password")
     
     if st.button("Générer le hash"):
-    ##      if password_input:
-    ##          # Convertit le texte en octets
-    ##          password_bytes = password_input.encode('utf-8')
+       if password_input:
+            # Convertit le texte en octets
+            password_bytes = password_input.encode('utf-8')
     ##          # Génère un sel et hache le mot de passe
     ##          salt = bcrypt.gensalt()
     ##          hashed = bcrypt.hashpw(password_bytes, salt)
     ##          # Retourne la chaîne encodée à stocker en base                 
     ##          hashed_result = hash_password(password_input) hashed.decode('utf-8') 
     ##        
-        st.success("Mot de passe haché avec succès !")
+            st.success("Mot de passe haché avec succès !")
            
     ##          # Affichage du résultat dans un bloc de code pour faciliter le copie-coller
     ##          st.code(hashed_result, language="text")
