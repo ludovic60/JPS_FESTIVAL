f337802fd1d7e154e7772158c1e46329e910627b
JPS_TMP <57634868+ludovic60@users.noreply.github.com>
2026-08-24 20:29:49 +0200

    Update config.py

--- app/bar_jeux_app/config.py
+++ app/bar_jeux_app/config.py
@@ -1,7 +1,7 @@
 """Configuration de l'application Bar à jeux."""
 from pathlib import Path
 import common_store as cs
-
+from common_store import _secrets
 ROOT_DIR = Path(__file__).resolve().parent.parent
 DATA_DIR = ROOT_DIR / "data" / "jeux"
 
