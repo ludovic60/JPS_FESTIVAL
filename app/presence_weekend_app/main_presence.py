bab36d3a1b01f1cb16f1e52046a3440bf7de43dc
JPS_TMP <57634868+ludovic60@users.noreply.github.com>
2026-08-25 21:12:59 +0200

    Update main_presence.py

--- app/presence_weekend_app/main_presence.py
+++ app/presence_weekend_app/main_presence.py
@@ -6,14 +6,18 @@ Lancement local :  streamlit run streamlit_app.py
 import streamlit as st
 import sys
 from pathlib import Path
-import auth, storage
+import storage
 from views import login_view, reset_password_view, main_app
 
 
 # Ajoute le dossier parent (la racine du projet) à sys.path
 racine_projet = Path(__file__).resolve().parent.parent
 sys.path.append(str(racine_projet))
-from design_system import inject
+from commun.design_system import inject
+from commun.auth import require_auth, logout
+
+# Exécute la vérification (affiche le formulaire si besoin, puis stoppe)
+require_auth()
 
 st.set_page_config(page_title="Présence Week-end", page_icon="📅", layout="wide")
 inject()
