2051e8536c6c423833e9342523b8b7b69643488c
JPS_TMP <57634868+ludovic60@users.noreply.github.com>
2026-08-26 15:33:42 +0200

    Update storage_presence.py

--- app/presence_weekend_app/storage_presence.py
+++ app/presence_weekend_app/storage_presence.py
@@ -130,7 +130,7 @@ def get_presence(user_id):
         db = cs.get_db()
         presence_tb = db.presence
         filtre_tb = {"annne": cs._secret("ANNEE_FESTIVAL") , "user_id" : ObjectId(user_id)}
-        selc_tb = { "creneau": 3,"task_ids": 4}
+        selc_tb = { "creneau": 1,"task_ids": 2}
         resultats = presence_tb.find(filtre_tb, selc_tb)
  
     else :
