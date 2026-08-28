12f006dd269b79f260638a3a05ffb3b5b67d16b3
JPS_TMP <57634868+ludovic60@users.noreply.github.com>
2026-08-28 15:59:17 +0200

    Update views.py

--- app/bar_jeux_app/views.py
+++ app/bar_jeux_app/views.py
@@ -111,7 +111,7 @@ def _game_card(g, list_key, user):
                         storage_jeux.toggle_suggestion(ckey, user["id"], val)
                         st.rerun()
             with cc[1]:
-                if "::" in ckey
+                if "::" in ckey :
                     periode, id_jeu = ckey.split("::") 
                     list_sugg_this_game = [game for game in sugg if game["id_jeux"] == id_jeu]
 
