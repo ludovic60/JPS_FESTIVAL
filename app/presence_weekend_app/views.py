6b61325c6c6105caf234fec4bf32ea7d31006aa5
JPS_TMP <57634868+ludovic60@users.noreply.github.com>
2026-09-02 18:58:26 +0200

    Update views.py

--- app/presence_weekend_app/views.py
+++ app/presence_weekend_app/views.py
@@ -306,10 +306,8 @@ def admin_page():
         if not users:
             st.info("Aucun utilisateur.")
             return
-        #options = {f"({u["pseudo"]}) ({u["email"]})" for u in users}
-        for u in users :
-            print(u)
-        options = {u["pseudo"] for u in users}
+        options = {f"({u["pseudo"]}) ({u["email"]})" for u in users}
+       
         
         choice = st.selectbox("Personne", list(options.keys()))
         target = options[choice]
