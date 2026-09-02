c0925ee1e9f9f927ac5a1aa3eba6d1bc3ad40e27
JPS_TMP <57634868+ludovic60@users.noreply.github.com>
2026-09-02 15:53:01 +0200

    Update views.py

--- app/presence_weekend_app/views.py
+++ app/presence_weekend_app/views.py
@@ -134,8 +134,9 @@ def presence_editor(user_id: str, user_name: str, key_prefix: str):
 
                     # Si la clé n'existe pas encore dans session_state, on l'initialise
                     if tkey not in st.session_state:
-                        print([item[0] for item in initial_taches if item[1] == day and item[0] == t_id] )
-                        st.session_state[tkey] = [item[0] for item in initial_taches if item[1] == day and item[0] == t_id][0]
+                        tache_coche = [item[0] for item in initial_taches if item[1] == day and item[0] == t_id] 
+                        if tache_coche :
+                            st.session_state[tkey] = tache_coche[0]
 
                     # ON NE PASSE PLUS value= ICI
                     st.checkbox(t["tache"], key=tkey)
