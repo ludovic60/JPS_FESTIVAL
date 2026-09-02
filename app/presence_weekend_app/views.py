a466aaefe32f656865304f6f4bc1b6b405a4006f
JPS_TMP <57634868+ludovic60@users.noreply.github.com>
2026-09-02 09:39:16 +0200

    Update views.py

--- app/presence_weekend_app/views.py
+++ app/presence_weekend_app/views.py
@@ -139,7 +139,7 @@ def presence_editor(user_id: str, user_name: str, key_prefix: str):
                if not tasks:
                         st.info("Aucune tâche disponible. L'administrateur doit en ajouter.")
                for t in tasks:
-                        for task in creneau_selc
+                        for task in creneau_selc :
                             task_ids.append( task[1])
                         checked = str(t["_id"]) in task_ids
                         st.checkbox(t["tache"], value=checked, key=f"task_{day}_task_{str(t['_id'])}"):
