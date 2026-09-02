b066c7521cf5a8d351f142dadb6a42b253e0f3f5
JPS_TMP <57634868+ludovic60@users.noreply.github.com>
2026-09-02 15:50:45 +0200

    Update views.py

--- app/presence_weekend_app/views.py
+++ app/presence_weekend_app/views.py
@@ -134,6 +134,7 @@ def presence_editor(user_id: str, user_name: str, key_prefix: str):
 
                     # Si la clé n'existe pas encore dans session_state, on l'initialise
                     if tkey not in st.session_state:
+                        print([item[0] for item in initial_taches if item[1] == day and item[0] == t_id] )
                         st.session_state[tkey] = [item[0] for item in initial_taches if item[1] == day and item[0] == t_id][0]
 
                     # ON NE PASSE PLUS value= ICI
