f5e906890733599b8507f81271b21d60c7dd3b09
JPS_TMP <57634868+ludovic60@users.noreply.github.com>
2026-09-03 20:53:22 +0200

    Update views.py

--- app/bar_jeux_app/views.py
+++ app/bar_jeux_app/views.py
@@ -277,9 +277,9 @@ def _final_page(user):
     for game in finals:
         print(game.get('id_jeux'))
         g = storage_jeux.get_info_games( game.get('id_jeux'))
-        print( int(str(g[0].get("annee"))))
-        print(int(str(g[0].get("mois")) ))
-        # if  ( int(str(g[0].get("annee")))*100 +  int(str((g[0].get("mois")) )))>    int( (cs._secret("ANNEE_FESTIVAL")-1)*100 + int(cs._secret("MOIS_FESTIVAL") ):
+        print( (str(g[0].get("annee"))))
+        print((str(g[0].get("mois_sortie")) ))
+        # if  ( int(str(g[0].get("annee")))*100 +  int(str((g[0].get("mois_sortie")) )))>    int( (cs._secret("ANNEE_FESTIVAL")-1)*100 + int(cs._secret("MOIS_FESTIVAL") ):
         #            New = "NOUVEAUTE"
         # else :   
         #            New = ""
