4732c478816a3c0cfed0ec8766b5a3632396b82d
JPS_TMP <57634868+ludovic60@users.noreply.github.com>
2026-09-03 21:35:52 +0200

    Update views.py

--- app/bar_jeux_app/views.py
+++ app/bar_jeux_app/views.py
@@ -284,10 +284,10 @@ def _final_page(user):
         logging.info(f"annee parution en int  {int(g[0].get("annee_parution"))}")
         logging.info(f"lmois de apurtion {g[0].get("mois_sortie")}")
         logging.info(f"lmois de parution en int {int(g[0].get("mois_sortie"))}")        
-        if  ( int(str(g[0].get("annee_parution")))*100 +  int(str((g[0].get("mois_sortie")) )))>    int( (cs._secret("ANNEE_FESTIVAL")-1)*100 + int(cs._secret("MOIS_FESTIVAL") ):
-                    New = "NOUVEAUTE"
-        else :   
-                    New = ""
+        # if  ( int(str(g[0].get("annee_parution")))*100 +  int(str((g[0].get("mois_sortie")) )))>    int( (cs._secret("ANNEE_FESTIVAL")-1)*100 + int(cs._secret("MOIS_FESTIVAL") ):
+        #            New = "NOUVEAUTE"
+        #else :   
+        #            New = ""
                
                
         row = {"nouveaute" : new, "Annee": g[0].get("annee"), "Categorie jeu": g[0].get("classement JPS final"), "Couverture Jeu": g[0].get("couverture"), "Jeu": g[0].get("nom_jeu_complet"), "Total coché": "" }
