4ebba24c63cf7b7b70293f9ad382e04116742c01
JPS_TMP <57634868+ludovic60@users.noreply.github.com>
2026-09-03 08:46:34 +0200

    Update views.py

--- app/bar_jeux_app/views.py
+++ app/bar_jeux_app/views.py
@@ -284,10 +284,10 @@ def _final_page(user):
                    New = ""
                
                
-        row = {"nouveaute" : new, "Annee": g[0].get("annee"), "Categorie jeu": g[0].get("classement JPS final"), "Couverture Jeu": g[0].get("couverture"), "Jeu": g[0].get("nom_jeu_complet"), "Total coché"= "" }
+        row = {"nouveaute" : new, "Annee": g[0].get("annee"), "Categorie jeu": g[0].get("classement JPS final"), "Couverture Jeu": g[0].get("couverture"), "Jeu": g[0].get("nom_jeu_complet"), "Total coché": "" }
         for u in users:
                    for ul in loans:   
-                              if ul[0].get("_id") =   u[0].get("_id") and    ul[0].get("_id")   == g[0].get("_id")  :  
+                              if ul[0].get("_id") ==   u[0].get("_id") and    ul[0].get("_id")   == g[0].get("_id")  :  
                                   row[u[0].get("pseudo")] = 1
         rows.append(row)
     df = pd.DataFrame(rows)
