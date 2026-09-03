0152e3bdc62d2fced91a1bd5103f93fa797183a3
JPS_TMP <57634868+ludovic60@users.noreply.github.com>
2026-09-03 06:03:56 +0200

    Update storage_jeux.py

--- app/bar_jeux_app/storage_jeux.py
+++ app/bar_jeux_app/storage_jeux.py
@@ -221,8 +221,9 @@ def final_games():
     if   con_mongo : 
         db = cs.get_db()
         game_selec_tb = db.selection_jeux_festival
+        selc_tb = {"id_jeux": 1, "_id": 1}
         filtre_tb = {"annee": cs._secret("ANNEE_FESTIVAL") }  
-        resultats = list(game_selec_tb.find(filtre_tb))
+        resultats = list(game_selec_tb.find(selc_tb , filtre_tb))
     return resultats 
 
 
