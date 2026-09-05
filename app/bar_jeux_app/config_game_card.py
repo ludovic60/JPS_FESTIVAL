53fe3ec4aa2c06555265698cfdccac2b7e717acd
JPS_TMP <57634868+ludovic60@users.noreply.github.com>
2026-09-05 22:23:38 +0200

    Update config_game_card.py

--- app/bar_jeux_app/config_game_card.py
+++ app/bar_jeux_app/config_game_card.py
@@ -1,6 +1,8 @@
 import config_bar_jeux
 import storage_jeux
 import streamlit as st
+import os
+import sys
 # Ajoute le dossier parent à sys.path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
