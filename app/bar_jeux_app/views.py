098112b5234658ae62469c1b00361fe217544899
JPS_TMP <57634868+ludovic60@users.noreply.github.com>
2026-09-06 00:27:15 +0200

    Update views.py

--- app/bar_jeux_app/views.py
+++ app/bar_jeux_app/views.py
@@ -409,7 +409,6 @@ def _final_page(user):
        gridOptions=grid_options,
        custom_css=custom_css, 
        theme="balham",  # Thème avec bordures et grille bien visibles
-       update_mode=GridUpdateMode.MODEL_CHANGED,
        allow_unsafe_jscode=True, ## pour gerer l'affichage des images grace aux url
        update_mode=GridUpdateMode.VALUE_CHANGED,  # Déclenche une mise à jour à chaque clic
        data_return_mode=DataReturnMode.AS_INPUT,
