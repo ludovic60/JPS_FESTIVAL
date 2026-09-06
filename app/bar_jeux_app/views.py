bd32e66e1d384bf97130e473a773339abd4725f6
JPS_TMP <57634868+ludovic60@users.noreply.github.com>
2026-09-06 23:24:14 +0200

    Update views.py

--- app/bar_jeux_app/views.py
+++ app/bar_jeux_app/views.py
@@ -260,9 +260,9 @@ def _final_page(user):
     def on_change_plusieurs_exemplaires(game_id, player_key, new_val):
           toggle_admin_selected(game_id, new_val)
     def on_change_prete(game_id, player_key, new_val):
-                   
+           return 1        
     def on_change_admin(game_id, player_key, new_val):
-                   
+           return 2         
  
     for game in finals:
         
