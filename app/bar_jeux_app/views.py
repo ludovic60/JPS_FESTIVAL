b25a29100b89518cc570a760a326071217aee0e2
JPS_TMP <57634868+ludovic60@users.noreply.github.com>
2026-09-07 14:05:10 +0200

    Update views.py

--- app/bar_jeux_app/views.py
+++ app/bar_jeux_app/views.py
@@ -440,6 +440,7 @@ def _final_page(user):
         data_return_mode=DataReturnMode.AS_INPUT,
         allow_unsafe_jscode=True,
         fit_columns_on_grid_load=False,
+        theme="balham",
         height=dynamic_height
     )
     
