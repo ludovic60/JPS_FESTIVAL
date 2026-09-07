49fa067a6fb6b0a21a17ff30f6ecacaa679ec053
JPS_TMP <57634868+ludovic60@users.noreply.github.com>
2026-09-07 09:10:24 +0200

    Update views.py

--- app/bar_jeux_app/views.py
+++ app/bar_jeux_app/views.py
@@ -301,7 +301,7 @@ def _final_page(user):
 
         row_jeux.append(row)
 
-     df_jeux = pd.DataFrame(row_jeux)
+    df_jeux = pd.DataFrame(row_jeux)
     if "df_jeux" not in st.session_state:         
           st.session_state["df_jeux"] = df_jeux 
  
