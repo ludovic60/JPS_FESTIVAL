2be39617990a9a801ce3a32d613122aae820119e
JPS_TMP <57634868+ludovic60@users.noreply.github.com>
2026-09-08 08:32:41 +0200

    Update config_game_card.py

--- app/bar_jeux_app/config_game_card.py
+++ app/bar_jeux_app/config_game_card.py
@@ -179,7 +179,7 @@ def _game_card(g, list_key, user):
           
          
             with st.expander(" Ajouter un commentaire"): 
-                 st.write(f"Ajouter une note pour : **{game_title}**")
+                    st.write(f"Ajouter une note pour : **{game_title}**")
                             
                     # Champ de saisie
                     texte = st.text_area("Votre commentaire :", key=f"txt_{ckey_this_game}")
