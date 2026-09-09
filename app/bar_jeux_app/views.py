e144bce21db29b9053d48b3607f4060403ae8fe2
JPS_TMP <57634868+ludovic60@users.noreply.github.com>
2026-09-09 20:00:23 +0200

    Update views.py

--- app/bar_jeux_app/views.py
+++ app/bar_jeux_app/views.py
@@ -208,7 +208,7 @@ def _final_page(user):
     ###########---- 0 dataframe pour alimenter les graph 
 
     liste_pret_validé = storage_jeux.get_validated_loans()
-    liste_info_valide={}
+    liste_info_valide=[]
     for game in  liste_pret_validé :
           info_games = storage_jeux.get_info_games(game["id_jeux"])
           for u in users :
@@ -219,7 +219,7 @@ def _final_page(user):
           liste_info_valide.append ({"classement":  info_games_pret["classement_jps_final"] ,  "Nouveauté":nouveaute_def(game["id_jeux"]), "pseudo":pseudo , "nom": info_games["nom_jeu_complet"],"Nb_jeux_valide":1, "statut_valide":True})
      
     df_jeux_valide_graphique  = pd.DataFrame(liste_info_valide)
-    liste_info={}
+    liste_info=[]
     liste_pret_user = storage_jeux.get_all_loans()
     for game_pret in  liste_pret_user :
           print(game_pret)
