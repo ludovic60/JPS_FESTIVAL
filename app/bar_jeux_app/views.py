27c09ea2c9d85b9e8bf4c9dd2e859e78b2805fc6
JPS_TMP <57634868+ludovic60@users.noreply.github.com>
2026-09-09 17:40:43 +0200

    Update views.py

--- app/bar_jeux_app/views.py
+++ app/bar_jeux_app/views.py
@@ -266,7 +266,7 @@ def _final_page(user):
           df_cochis = df_jeux_histogramme[df_jeux_histogramme["Valeur"] > 0]
           if not df_cochis.empty:
               df_nov = (
-                  df_jeux_valide_graphique[Nb_jeux"]
+                  df_jeux_valide_graphique["Nb_jeux"]
                   .map({True: "Nouveauté", False: "Ancien"})
                   .value_counts()
                   .reset_index()
