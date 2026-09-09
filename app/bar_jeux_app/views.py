3cee3418b5ecc886aeb834ba90690f8737e3bb76
JPS_TMP <57634868+ludovic60@users.noreply.github.com>
2026-09-09 18:46:24 +0200

    Update views.py

--- app/bar_jeux_app/views.py
+++ app/bar_jeux_app/views.py
@@ -240,8 +240,8 @@ def _final_page(user):
             
           st.subheader("Validations par Joueur")
           
-          df_jeux_histogramme  = pd.merge(df_jeux_pret_graphique, df_jeux_valide_graphique, on =["classement", "Nouveauté", "pseudo","nom" ]  , how="left")
-               
+         ### df_jeux_histogramme  = pd.merge(df_jeux_pret_graphique, df_jeux_valide_graphique, on =["classement", "Nouveauté", "pseudo","nom" ]  , how="left")
+          df_jeux_histogramme  = df_jeux_pret_graphique
           fig_hist = px.bar(
                df_jeux_valide_graphique,
               x="pseudo",
