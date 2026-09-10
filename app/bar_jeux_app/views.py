da319c887106b5322461e1c366d9d12a2dc565ed
JPS_TMP <57634868+ludovic60@users.noreply.github.com>
2026-09-10 10:10:02 +0200

    Update views.py

--- app/bar_jeux_app/views.py
+++ app/bar_jeux_app/views.py
@@ -340,7 +340,8 @@ def _final_page(user):
         result = False 
         
         for id in liste_jeu_plusieurs_exemplaire :
-          if game_id== id :
+           print(f" id jeux {id["id_jeux"]}")
+          if game_id== id["id_jeux"] :
             print(f"passage boucle vrai {game_id}  et {result}") 
             result = True
           else :
