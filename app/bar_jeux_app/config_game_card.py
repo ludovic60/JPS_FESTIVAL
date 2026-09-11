3f681063f008f6d8bb49e7ca4607962808defcf3
JPS_TMP <57634868+ludovic60@users.noreply.github.com>
2026-09-11 06:59:43 +0200

    Update config_game_card.py

--- app/bar_jeux_app/config_game_card.py
+++ app/bar_jeux_app/config_game_card.py
@@ -151,7 +151,7 @@ def _game_card(g, list_key, user):
                     
                     # 5. Détection du clic réel (changement d'état pour ce jeu précis)
                     if val_check_suggest != has_suggested:
-                        storage_jeux.toggle_suggestion(ckey_this_game, user["id"], val)
+                        storage_jeux.toggle_suggestion(ckey_this_game, user["id"], val_check_suggest)
                         st.rerun()
                 
                    
