cbf02d8d884ea675355b98bfd50be6fe89b4120e
JPS_TMP <57634868+ludovic60@users.noreply.github.com>
2026-09-11 17:15:13 +0200

    Update views.py

--- app/bar_jeux_app/views.py
+++ app/bar_jeux_app/views.py
@@ -366,9 +366,7 @@ def _final_page(user):
     print(f"liste jeu pete : {list_jeu_prete}")
     print(f"liste jeu valide : {list_jeu_prete_valide}")
     def get_prete_value(game_id, player_key):
-      pseudo_pret = any  (u["pseudo"] for u in users)
-
-      pseudos = [u['pseudo'] for u in users if u['_id'] == pret['user_id']]
+      pseudos = [u['pseudo'] for u in users if u['_id'] == player_key]
       print(f"le pseudo")
       print(pseudos)
       result = any( pseudos[0] == player_key and pret['id_jeux'] == game_id 
@@ -378,7 +376,7 @@ def _final_page(user):
       return result
     
     def get_admin_valide_value(game_id, player_key):
-      pseudos = [u['pseudo'] for u in users if u['_id'] == pret['user_id']]
+      pseudos = [u['pseudo'] for u in users if u['_id'] == player_key]
       print("le pseudo")           
       print( pseudos)
       result = any( pseudos[0] == player_key and pret['id_jeux'] == game_id 
