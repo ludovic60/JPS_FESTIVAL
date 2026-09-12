ebd87c57eb86b93af2c6f72056139c06ebea8d44
JPS_TMP <57634868+ludovic60@users.noreply.github.com>
2026-09-12 06:49:02 +0200

    Update views.py

--- app/bar_jeux_app/views.py
+++ app/bar_jeux_app/views.py
@@ -431,7 +431,7 @@ def _final_page(user):
     
         for idx, j in  sorted(enumerate(pseudo_list), key=lambda item: item[1]) :
             
-            player_key = f"j{idx+1}"
+            player_key = f"{j}[idx+1}"
             row[f"{player_key}_prete"] = bool(get_prete_value(game_id, j))  # <-- vrai bool
             row[f"{player_key}_admin"] = bool(get_admin_valide_value(game_id, j))  # <-- vrai bool
 
@@ -662,40 +662,7 @@ def _final_page(user):
     new_df = pd.DataFrame(grid_response["data"])
     
     # 2. Détection du changement
-    if "old_grid_df" in st.session_state:
-        old_df = st.session_state["old_grid_df"]
-    
-        if len(old_df) == len(new_df):
-            checkbox_cols = [
-                c
-                for c in new_df.columns
-                if c.endswith(("_prete", "_admin"))
-                or c == "Plusieurs exemplaires souhaités"
-            ]
-    
-            # Recherche de la cellule modifiée
-            for i in new_df.index:
-                game_id = new_df.at[i, "_id"]
-                for col in checkbox_cols:
-                    old_val = old_df.at[i, col]
-                    new_val = new_df.at[i, col]
-    
-                    if bool(old_val) != bool(new_val):
-                        st.write(
-                            f"Changement détecté sur {col} pour le jeu {game_id} : {old_val} -> {new_val}"
-                        )
-    
-                        if col.endswith("_prete"):
-                            player_key = col.replace("_prete", "")
-                            on_change_prete(game_id, player_key, new_val)
-                        elif col.endswith("_admin"):
-                            player_key = col.replace("_admin", "")
-                            on_change_admin(game_id, player_key, new_val)
-                        elif col == "Plusieurs exemplaires souhaités":
-                            on_change_plusieurs_exemplaires(game_id, new_val)
-    
-    # 3. Mettre à jour l'état précédent pour le prochain rerun
-    st.session_state["old_grid_df"] = new_df.copy()
+
  
 
 
