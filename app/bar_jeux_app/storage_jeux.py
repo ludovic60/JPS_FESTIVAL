a2df2e425d1f923084a4b7df26e5c3ff222a08ce
JPS_TMP <57634868+ludovic60@users.noreply.github.com>
2026-09-13 18:26:16 +0200

    Update storage_jeux.py

--- app/bar_jeux_app/storage_jeux.py
+++ app/bar_jeux_app/storage_jeux.py
@@ -150,10 +150,10 @@ def get_suggestions(mode):
     if   con_mongo : 
         db = cs.get_db()
         game_suggest_tb = db.jeux_suggestions
-            if mode == "pret" :
-                filtre_tb = {"annee": cs._secret("ANNEE_FESTIVAL" , "prete": "True" }
-            else :
-                filtre_tb = {"annee": cs._secret("ANNEE_FESTIVAL" , "prete": "False" }
+        if mode == "pret" :
+             filtre_tb = {"annee": cs._secret("ANNEE_FESTIVAL" , "prete": "True" }
+        else :
+             filtre_tb = {"annee": cs._secret("ANNEE_FESTIVAL" , "prete": "False" }
         
         resultats = list(game_suggest_tb.find(filtre_tb))
     else :
