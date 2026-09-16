5fa3751d9591a8d6869977d7a0bfe6f98fb9e677
JPS_TMP <57634868+ludovic60@users.noreply.github.com>
2026-09-17 00:27:59 +0200

    Update config_game_card.py

--- app/bar_jeux_app/config_game_card.py
+++ app/bar_jeux_app/config_game_card.py
@@ -246,7 +246,7 @@ def _game_card(g, list_key, user , mode ):
                                 st.write(f"liste des personnes souhaitant préter le jeu")
                                 for id_user in liste_preteur :
                                         pseudo  =  (u["pseudo"]  for u in user if  u["id"]==id_user.get("user_id") )
-                                        s.write(id_user.get("user_id"))
+                                        st.write(id_user.get("user_id"))
                                         st.write(f"- {pseudo}")
                    
                 else :                        
