250275261c1d148faad475feab206df1ed728e02
JPS_TMP <57634868+ludovic60@users.noreply.github.com>
2026-09-16 12:17:56 +0200

    Update views.py

--- app/bar_jeux_app/views.py
+++ app/bar_jeux_app/views.py
@@ -160,7 +160,7 @@ def _requests_suggestion_page(user):
     list_suggest = storage_jeux.get_game_suggestions_a_traiter()
     list_games =[]
     for sugg in list_suggest :
-        list_games.append(storage_jeu.get_info_games(sugg.get("id_jeux")))
+        list_games.append(storage_jeux.get_info_games(sugg.get("id_jeux")))
      
     if list_games:
         per_row = 3
