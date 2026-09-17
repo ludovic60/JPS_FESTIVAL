78d3ccf5b60e3579989cd6205aea5d8ed9d6535e
JPS_TMP <57634868+ludovic60@users.noreply.github.com>
2026-09-17 21:51:22 +0200

    Update views.py

--- app/bar_jeux_app/views.py
+++ app/bar_jeux_app/views.py
@@ -12,6 +12,7 @@ import storage_jeux
 import export
 import os
 import sys
+from bson import ObjectId
 from config_game_card import _game_card , mise_forme_classement , nouveaute_def
 # Ajoute le dossier parent à sys.path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
