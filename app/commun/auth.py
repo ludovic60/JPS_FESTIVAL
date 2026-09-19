39810ce5a254129b9134181a6f506199a73c6910
JPS_TMP <57634868+ludovic60@users.noreply.github.com>
2026-09-19 02:42:45 +0200

    Update auth.py

--- app/commun/auth.py
+++ app/commun/auth.py
@@ -17,6 +17,37 @@ from commun.config import get_secret
 from commun.security import generate_token
 
 
+
+import tomllib
+#####------------------------------------------------------------------------------------------------------------
+####force le mode d'affichage definie dans tomlib  dans les differents navigateur (opera et chrome par exemple)
+# Charger le fichier de configuration Streamlit
+try:
+  with open(".streamlit/config.toml", "rb") as f:
+    config = tomllib.load(f)
+  bg_color = config.get("theme", {}).get("backgroundColor", "#FFFFFF")
+  text_color = config.get("theme", {}).get("textColor", "#262730")
+except Exception:
+  # Valeurs par défaut de secours en cas d'erreur
+  bg_color = "#FFFFFF"
+  text_color = "#262730"
+
+st.markdown(
+    f"""
+    <style>
+    .stApp {{
+        background-color: {bg_color};
+        color: {text_color};
+    }}
+    </style>
+""",
+    unsafe_allow_html=True,
+)
+
+
+#####------------------------------------------------------------------------------------------------------------
+
+
 controller = CookieController()
 
 
