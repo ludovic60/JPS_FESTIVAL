0738b75e4301dc51aa55d390aeb3b3f82b169d4d
JPS_TMP <57634868+ludovic60@users.noreply.github.com>
2026-09-19 02:39:50 +0200

    Update jeux_app.py

--- app/bar_jeux_app/jeux_app.py
+++ app/bar_jeux_app/jeux_app.py
@@ -9,6 +9,34 @@ import storage_jeux
 import os
 import sys
 
+import tomllib
+#####------------------------------------------------------------------------------------------------------------
+####force le mode d'affichage definie dans tomlib  dans les differents navigateur (opera et chrome par exemple)
+# Charger le fichier de configuration Streamlit
+try:
+  with open(".streamlit/config.toml", "rb") as f:
+    config = tomllib.load(f)
+  bg_color = config.get("theme", {}).get("backgroundColor", "#FFFFFF")
+  text_color = config.get("theme", {}).get("textColor", "#262730")
+except Exception:
+  # Valeurs par défaut de secours en cas d'erreur
+  bg_color = "#FFFFFF"
+  text_color = "#262730"
+
+st.markdown(
+    f"""
+    <style>
+    .stApp {{
+        background-color: {bg_color};
+        color: {text_color};
+    }}
+    </style>
+""",
+    unsafe_allow_html=True,
+)
+
+
+#####------------------------------------------------------------------------------------------------------------
 
 # Ajoute le dossier parent (la racine du projet) à sys.path
 racine_projet = Path(__file__).resolve().parent.parent
