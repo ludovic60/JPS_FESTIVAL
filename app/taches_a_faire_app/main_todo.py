43a565b99321b4117061de7dd2f9232f789c878f
JPS_TMP <57634868+ludovic60@users.noreply.github.com>
2026-09-19 02:43:48 +0200

    Update main_todo.py

--- app/taches_a_faire_app/main_todo.py
+++ app/taches_a_faire_app/main_todo.py
@@ -16,6 +16,35 @@ sys.path.append(str(racine_projet))
 from commun.design_system import inject
 from commun.auth import require_auth, logout, current_user
 
+import tomllib
+#####------------------------------------------------------------------------------------------------------------
+####force le mode d'affichage definie dans tomlib  dans les differents navigateur (opera et chrome par exemple)
+# Charger le fichier de configuration Streamlit
+try:
+  with open(".streamlit/config.toml", "rb") as f:
+    config = tomllib.load(f)
+  bg_color = config.get("theme", {}).get("backgroundColor", "#FFFFFF")
+  text_color = config.get("theme", {}).get("textColor", "#262730")
+except Exception:
+  # Valeurs par défaut de secours en cas d'erreur
+  bg_color = "#FFFFFF"
+  text_color = "#262730"
+
+st.markdown(
+    f"""
+    <style>
+    .stApp {{
+        background-color: {bg_color};
+        color: {text_color};
+    }}
+    </style>
+""",
+    unsafe_allow_html=True,
+)
+
+
+#####------------------------------------------------------------------------------------------------------------
+
 # Exécute la vérification (affiche le formulaire si besoin, puis stoppe)
 require_auth()
 
