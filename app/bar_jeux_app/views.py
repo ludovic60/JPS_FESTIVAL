fb062e5a30d7040c698ee7422c8793f63376ac45
JPS_TMP <57634868+ludovic60@users.noreply.github.com>
2026-09-23 12:45:12 +0200

    Update views.py

--- app/bar_jeux_app/views.py
+++ app/bar_jeux_app/views.py
@@ -550,10 +550,10 @@ def _final_page(user):
    
         # Ajout à la liste
         liste_info.append({
-            "classement": game_pret["classement_jps_final"],
+            "classement": game_pret[0]["classement_jps_final"],
             "Nouveauté": nouveaute_def(game_pret),
-            "pseudo": game_pret["pseudo"],
-            "nom": info_games_pret["nom_jeu_complet"],
+            "pseudo": game_pret[0]["pseudo"],
+            "nom": info_games_pret[0]["nom_jeu_complet"],
             "Nb_jeux_propose": 1,
         })
     df_jeux_pret_graphique  = pd.DataFrame(liste_info)
