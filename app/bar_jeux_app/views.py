71b8126afc343a30c8ced0e40a81dffaeeee02ec
JPS_TMP <57634868+ludovic60@users.noreply.github.com>
2026-09-23 11:09:40 +0200

    Update views.py

--- app/bar_jeux_app/views.py
+++ app/bar_jeux_app/views.py
@@ -529,7 +529,7 @@ def _final_page(user):
     liste__info_pret_user = pd.DataFrame(storage_jeux.load_games(liste_game, None))
     liste_pret_user_pd = pd.DataFrame(liste_pret_user)
  
-    liste__info_pret_user = liste__info_pret_user.rename(columns={'_id': 'id_jeux'})
+    liste__info_pret_user = liste__info_pret_user.rename(columns={'id': 'id_jeux'})
 
 
     liste_pret_user_detail = pd.merge(
