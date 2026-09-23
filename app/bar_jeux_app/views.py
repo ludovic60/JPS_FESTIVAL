2a24ae5901f51eeada6dcb7a00d356dd7ee90460
JPS_TMP <57634868+ludovic60@users.noreply.github.com>
2026-09-23 18:31:41 +0200

    Update views.py

--- app/bar_jeux_app/views.py
+++ app/bar_jeux_app/views.py
@@ -527,7 +527,7 @@ def _final_page(user):
     liste_info = []
     liste_pret_user = storage_jeux.get_all_loans()
     liste_game_preter = [ObjectId(game["id_jeux"]) for game in liste_pret_user ]
-          
+    print(liste_game_preter)      
     liste_detail = storage_jeux.load_games(liste_game_preter, None)
     print("liste_detail")
  
