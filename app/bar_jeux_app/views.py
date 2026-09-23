61763fced3cd2c55bac1e5393db07dec3ad06ced
JPS_TMP <57634868+ludovic60@users.noreply.github.com>
2026-09-24 00:46:23 +0200

    Update views.py

--- app/bar_jeux_app/views.py
+++ app/bar_jeux_app/views.py
@@ -508,7 +508,7 @@ def _final_page(user):
     ###########---- 0 dataframe pour alimenter les graph 
     # 1. Optimisation : création d'un dictionnaire d'utilisateurs {str(id): pseudo}
     ##users_dict = [ u for u in users]
-    users_dict_pd = pd.dataframe(users)
+    users_dict_pd = pd.DataFrame(users)
 
 
 
