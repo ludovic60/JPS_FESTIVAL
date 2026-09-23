115dc1a805b597a3e57abf8b19709d321fbb8492
JPS_TMP <57634868+ludovic60@users.noreply.github.com>
2026-09-23 17:19:01 +0200

    Update views.py

--- app/bar_jeux_app/views.py
+++ app/bar_jeux_app/views.py
@@ -556,8 +556,10 @@ def _final_page(user):
     print("info jeu prete")
     print( liste__info_pret_user[liste__info_pret_user["id_jeux"] == "9e705c422573f38168868b69"])
 
-    liste_pret_user_detail["Nouveauté"] = liste_pret_user_detail.apply(nouveaute_def, axis=1)
-    liste_pret_user_detail["Nb_jeux_propose"] = 1
+
+    if not liste__info_pret_user.empty:
+         liste_pret_user_detail["Nouveauté"] = liste_pret_user_detail.apply(nouveaute_def, axis=1)
+         liste_pret_user_detail["Nb_jeux_propose"] = 1
 
     df_jeux_pret_graphique  = liste_pret_user_detail   
     print( df_jeux_pret_graphique)
