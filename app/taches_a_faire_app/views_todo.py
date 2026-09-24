5efb3c285d4d95eacbd58874a6fb7bed238ff814
JPS_TMP <57634868+ludovic60@users.noreply.github.com>
2026-09-24 02:57:17 +0200

    Update views_todo.py

--- app/taches_a_faire_app/views_todo.py
+++ app/taches_a_faire_app/views_todo.py
@@ -54,6 +54,10 @@ def todo_page(user):
                         if c2.form_submit_button("Ajouter", type="primary") and new_label.strip():
                             add_todo(new_label)
                             st.rerun()
+       
+    
+
+       st.markdown(" - TACHES RESTANT A FAIRE ")
        c1, c2, c3,c4 ,c5, c6= st.columns([4, 4, 1, 1, 1,1])
        c1.write("nom de la tache")
        c2.write("affecte a ")
@@ -61,7 +65,7 @@ def todo_page(user):
        c4.write("modifier")
        c5.write("terminer ")
        c6.write("supprimer ")                     
-       for t in get_todo():
+       for t in get_todo("OPEN"):
 
            
             label_todo = c1.text_input("t", value=t["todo"], key=f"edit_{t["_id"]}",
@@ -85,4 +89,23 @@ def todo_page(user):
                 delete_todo(t["_id"])              
                 get_todo()
                 st.rerun()
+
+       st.markdown(" - TACHES TERMINEE ")
+       c1, c2, c3= st.columns([4, 4, 1, 1, 1,1])
+       c1.write("nom de la tache")
+       c2.write("affecte a ")
+       c3.write("statut")
+                     
+       for t in get_todo("CLOSED"):
+
+           
+            label_todo = c1.text_input("t", value=t["todo"], key=f"edit_{t["_id"]}",
+                                   label_visibility="collapsed")
+            label_affecte = c2.text_input("t", value=t["affecte"], key=f"edit_qui_{t["_id"]}",
+                                   label_visibility="collapsed")
+            label_statut = c3.text_input("t", value=t["statut"], key=f"edit_statut_{t["_id"]}",
+                                   label_visibility="collapsed")
+            
+           
+
 
