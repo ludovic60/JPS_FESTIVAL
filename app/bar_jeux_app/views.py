0b57fb831e1ccf6ab3d539ead32d27c0b5432fad
JPS_TMP <57634868+ludovic60@users.noreply.github.com>
2026-09-24 14:13:10 +0200

    Update views.py

--- app/bar_jeux_app/views.py
+++ app/bar_jeux_app/views.py
@@ -354,7 +354,8 @@ def _final_page(user):
 
     with col_btn4 :  
           df_export_list_initiale = df_jeux[["Couverture Jeu","Jeu" ]]
-          write(df_export_list_initiale)
+          print("df_export_list_initiale")
+          print(df_export_list_initiale)
           excel_data_initial = to_excel(df_export_list_initiale , "liste_selection_jeu.xlsx") 
     
           export_list_perso_button = st.download_button(
