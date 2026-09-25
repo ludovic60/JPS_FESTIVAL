5fd21949db29300c9e4052a283b9750fd5b25ea9
JPS_TMP <57634868+ludovic60@users.noreply.github.com>
2026-09-25 15:02:38 +0200

    Update views.py

--- app/presence_weekend_app/views.py
+++ app/presence_weekend_app/views.py
@@ -67,11 +67,9 @@ def presence_editor(user_id: str, user_name: str, key_prefix: str):
 
     # 1. CHARGEMENT INITIAL (Exécuté une seule fois par session utilisateur)
     bdd_key = f"db_loaded_{key_prefix}_{user_id}"
-    st.write(user_id)
 
     if bdd_key not in st.session_state:
         creneau_selc = get_presence(user_id)
-        st.write(creneau_selc)
         periode_db = set()
         taches_db = set()
 
